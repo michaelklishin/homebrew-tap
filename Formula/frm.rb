c29315c3436b8782b8280b9d44488fb1ddf1cc4f
Michael Klishin <michaelklishin@icloud.com>
2026-02-18 18:12:12 -0800

    frm 0.20.0
    Conflicts:
    	Formula/frm.rb

--- Formula/frm.rb
+++ Formula/frm.rb
@@ -23,23 +23,24 @@
 class Frm < Formula
   desc "RabbitMQ version manager based on generic binary builds"
   homepage "https://github.com/michaelklishin/frm"
-  version "0.19.0"
+  version "0.20.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/michaelklishin/frm/releases/download/v0.19.0/frm-0.19.0-aarch64-apple-darwin.tar.gz"
-      sha256 "938cc30a2c22937baa465b849f38851f588950cc3ec8ac88a201ab33ea2f7380"
+      url "https://github.com/michaelklishin/frm/releases/download/v0.20.0/frm-0.20.0-aarch64-apple-darwin.tar.gz"
+      sha256 "b532bedd6255bf0688bfd296eeff44edd6cfeb0282820282210060ba63957dd3"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/michaelklishin/frm/releases/download/v0.19.0/frm-0.19.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d58971fd07503631278886c575367cf80d445a27f2b0c09fd0a76014eeee7656"
-    else
-      url "https://github.com/michaelklishin/frm/releases/download/v0.19.0/frm-0.19.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2933695ea9daee9adabd4ef6a57ec82e5f74acc27bb9bac30f2ce538ed4f9b7c"
+    on_arm do
+      url "https://github.com/michaelklishin/frm/releases/download/v0.20.0/frm-0.20.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0358861966687cf00222c5dfb20a46d51b35a38a15cfed8b859106bd94d1e35b"
+    end
+    on_intel do
+      url "https://github.com/michaelklishin/frm/releases/download/v0.20.0/frm-0.20.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "f9945caf073e31423d29c0dadceda357549a88788e39a1ee2fe229e25b7f27d5"
     end
   end
 
